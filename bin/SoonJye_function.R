37fbe7830980b7b7b89057dc3d53f48917d965b1
Bo Wen <wenbostar@gmail.com>
2020-04-07 22:07:35 -0500

    Updated clinical file loading function

--- bin/SoonJye_function.R
+++ bin/SoonJye_function.R
@@ -87,6 +87,7 @@ run_2b <- function(pro_file, rna_file, anno_file, gene_file, out_dir="./",
   for(i in 1:length(clinical_attributes)){
     #cli_attr <- clinical_attributes[i]
     cat("clinical attributes: ",clinical_attributes[i],"\n")
+    traincli[,cli_attr_prob_names_true[i]] <- as.factor(traincli[,cli_attr_prob_names_true[i]])
     traincli[,cli_attr_prob_names_true[i]] <- apply(traincli, 1, function(x) if (x[clinical_attributes[i]] == levels(traincli[,clinical_attributes[i]])[1]) 0 else 1)
   }
   
